f2a1574f9c7d4027904e452f5a1dc53efcdf6cc3
Mamadou Soumahoro <memdydiop@gmail.com>
2026-09-19 09:40:22 +0000

    SIBEA V2.6 : polish admin (carte sans bordure, inputs flux, fond sidebar)

--- resources/views/components/admin/card.blade.php
+++ resources/views/components/admin/card.blade.php
@@ -3,11 +3,11 @@
     'subtitle' => null,
 ])
 
-<flux:card {{ $attributes->merge(['class' => 'p-0']) }}>
+<flux:card {{ $attributes->merge(['class' => 'p-0 rounded-md border-none! shadow-[0_1px_2px_rgba(57,62,80,0.15)]!']) }}>
     @if($title)
-        <div class="flex flex-wrap items-center justify-between gap-3 border-b border-bordure px-5 py-3">
+        <div class="flex flex-wrap items-center justify-between gap-3 border-b border-bordure border-dashed px-5 py-3">
             <div>
-                <flux:heading size="lg">{{ $title }}</flux:heading>
+                <flux:heading size="lg" class="font-medium">{{ $title }}</flux:heading>
                 @if($subtitle)
                     <flux:subheading>{{ $subtitle }}</flux:subheading>
                 @endif
--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -3,7 +3,7 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="theme-admin min-h-screen bg-white dark:bg-zinc-800">
+    <body class="theme-admin min-h-screen bg-[#f6f7fb] dark:bg-zinc-800">
         <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.header>
                 <x-app-logo :sidebar="true" href="{{ route('admin.dashboard') }}" wire:navigate />
